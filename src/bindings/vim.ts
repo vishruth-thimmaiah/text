a40ab4dcfbc96484459d859f3081fcd7e9dc466f
Vishruth-Thimmaiah <vishruththimmaiah@gmail.com>
2024-08-04 19:31:23 +0530

    feat: mouse and arrow keys now move the cursor

---
+++ src/bindings/vim.ts
@@ -0,0 +1,13 @@
+
+// if (event.key === "l") {
+// 	cursor.style.left = `${Number(left_incr) + 8}px`
+// }
+// else if (event.key === "h") {
+// 	cursor.style.left = `${Number(left_incr) - 8}px`
+// }
+// else if (event.key === "j") {
+// 	cursor.style.top = `${Number(top_incr) + 18}px`
+// }
+// else if (event.key === "k") {
+// 	cursor.style.top = `${Number(top_incr) - 18}px`
+// }
--- src/views/editor.vue
+++ src/views/editor.vue
@@ -2,14 +2,14 @@
 	<button @click="open_file">Open File</button>
 	<Cursor id="cursor" />
 	<div>
-		<Line :text="line" v-for="line in lines" />
+		<Line @click="mouse_click" :text="line" v-for="line in lines" />
 	</div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 import Line from '../components/line.vue';
-import { invoke } from '@tauri-apps/api';
+import { event, invoke } from '@tauri-apps/api';
 import { open } from '@tauri-apps/api/dialog';
 import Cursor from '../components/cursor.vue';
 
@@ -22,8 +22,6 @@ async function load(file: string) {
 	await invoke("open_file", { filepath: file })
 
 	lines.value = await invoke<string[]>("file_lines", { fileIndex: 0, startPos: 0, endPos: 100 })
-
-	await invoke("add_chars", { chars: "string", startLine: 1, startPoint: 3 })
 }
 
 onMounted(async () => {
@@ -46,17 +44,53 @@ onkeydown = async (event) => {
 	const top_incr = (window.getComputedStyle(cursor)).top.match(/(\d)+/g)!
 	if (event.key.length === 1) {
 
-		const cursor_pos_column = (Number(left_incr) - 7) / 8
-		const cursor_pos_row = (Number(top_incr) - 34) / 18
+		const cursor_pos_column = Math.floor((Number(left_incr) - 6) / 8)
+		const cursor_pos_row = Math.floor((Number(top_incr) - 34) / 18)
 
-		await invoke("add_chars", { chars: event.key, startLine: cursor_pos_row, startPoint: cursor_pos_column })
+		console.log(cursor_pos_row)
+
 		lines.value[cursor_pos_row] = lines.value[cursor_pos_row].substring(0, cursor_pos_column) + event.key + lines.value[cursor_pos_row].substring(cursor_pos_column)
+		await invoke("add_chars", { chars: event.key, startLine: cursor_pos_row, startPoint: cursor_pos_column })
 
 		cursor.style.left = `${Number(left_incr) + 8}px`
 	}
 
-	if (event.key === "ArrowDown") {
-		cursor.style.top = `${Number(top_incr) + 18}px`
+	else {
+		switch (event.key) {
+			case "ArrowDown":
+				cursor.style.top = `${Number(top_incr) + 18}px`
+				break;
+			case "ArrowUp":
+				cursor.style.top = `${Number(top_incr) - 18}px`
+				break;
+			case "ArrowLeft":
+				cursor.style.left = `${Number(left_incr) - 8}px`
+				break;
+			case "ArrowRight":
+				cursor.style.left = `${Number(left_incr) + 8}px`
+				break;
+
+			default:
+				break;
+		}
+
+	}
+}
+
+onclick = (event) => {
+	const cursor = document.getElementById("cursor")!
+	cursor.style.top = `${round(event.y, 18)}px`
+	cursor.style.left = `${round(event.x, 8)}px`
+}
+
+
+function round(number: number, multiple: number) {
+	const mod = number % multiple
+	if (mod < (multiple / 2)) {
+		return number - mod
+	}
+	else {
+		return number + (multiple - mod)
 	}
 }
 </script>
@@ -72,6 +106,6 @@ div {
 	/* 18px */
 	top: calc(34px + 0px);
 	/* 8px */
-	left: calc(7px + 0px);
+	left: calc(6px + 0px);
 }
 </style>
