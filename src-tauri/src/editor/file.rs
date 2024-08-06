8c0490f0f6e6bb54c4f860781a2ebfc734c45497
Vishruth-Thimmaiah <vishruththimmaiah@gmail.com>
2024-08-06 16:14:02 +0530

    refactor: seperate editor commands from main.rs

---
+++ src-tauri/src/editor/file.rs
@@ -0,0 +1,98 @@
+use std::fs::File;
+use std::{fs, sync::Mutex};
+
+use tauri::State;
+
+use crate::{InnerAppState, OpenFiles};
+
+#[tauri::command]
+pub fn list_files(dir: String, state: State<'_, Mutex<InnerAppState>>) -> (Vec<String>, Vec<String>) {
+    let files = fs::read_dir(&dir).unwrap();
+    state.lock().unwrap().active_dir = Some(dir);
+
+    let mut file_names = Vec::new();
+    let mut dir_names = Vec::new();
+
+    for file in files {
+        if let Ok(file) = file {
+            let path = file.path();
+            let pathname = path
+                .file_name()
+                .unwrap()
+                .to_str()
+                .map(|s| s.to_owned())
+                .unwrap();
+            if path.is_file() {
+                file_names.push(pathname)
+            } else if path.is_dir() {
+                dir_names.push(pathname)
+            }
+        }
+    }
+
+    return (file_names, dir_names);
+}
+
+#[tauri::command]
+pub fn open_file(filepath: String, state: State<'_, Mutex<InnerAppState>>) {
+    let rope = ropey::Rope::from_reader(File::open(&filepath).unwrap()).unwrap();
+
+    state.lock().unwrap().files.push(OpenFiles {
+        filepath,
+        rope: Mutex::new(rope),
+    });
+}
+
+#[tauri::command]
+pub fn file_lines(
+    file_index: usize,
+    mut start_pos: usize,
+    end_pos: usize,
+    state: State<'_, Mutex<InnerAppState>>,
+) -> Vec<String> {
+    let files = state.lock().unwrap();
+    let file = files.files.get(file_index).unwrap();
+
+    let mut lines: Vec<String> = vec![];
+    let binding = file.rope.lock().unwrap();
+    if let Some(mut line) = binding.get_lines_at(start_pos) {
+        while start_pos < end_pos {
+            let next_line = line.next();
+            if next_line == None {
+                break;
+            }
+            lines.push(next_line.expect("Error reading file").to_string());
+            start_pos += 1;
+        }
+    }
+
+    lines
+}
+
+#[tauri::command]
+pub fn add_chars(
+    file_index: usize,
+    chars: String,
+    start_line: usize,
+    start_point: usize,
+    state: State<'_, Mutex<InnerAppState>>,
+) {
+    let files = &state.lock().unwrap();
+    let file = files.files.get(file_index).unwrap();
+    let mut rope = file.rope.lock().unwrap();
+    let line_index = rope.line_to_char(start_line);
+    let line_end_index = rope.line_to_char(start_line + 1);
+
+    let mut new_line = rope.line(start_line).to_string();
+
+    new_line.insert_str(start_point, &chars);
+
+    rope.remove(line_index..line_end_index);
+    rope.insert(line_index, &new_line);
+
+    file.rope
+        .lock()
+        .unwrap()
+        .write_to(File::create(&file.filepath).unwrap())
+        .unwrap();
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,119 +1,36 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{
-    fs::{self, File, ReadDir},
-    sync::Mutex,
-};
+mod editor;
+
+use editor::file::{add_chars, file_lines, list_files, open_file};
+use std::sync::Mutex;
 
 use ropey::Rope;
-use tauri::State;
 
 struct OpenFiles {
     filepath: String,
     rope: Mutex<Rope>,
 }
 
-struct AppState {
-    files: Mutex<Vec<OpenFiles>>,
-    active_dir: Option<ReadDir>,
+struct InnerAppState {
+    files: Vec<OpenFiles>,
+    active_dir: Option<String>,
 }
 
-#[tauri::command]
-fn list_files(dir: String, state: State<AppState>) -> (Vec<String>, Vec<String>) {
-    let files = fs::read_dir(dir).unwrap();
-
-    let mut file_names = Vec::new();
-    let mut dir_names = Vec::new();
-
-    for file in files {
-        if let Ok(file) = file {
-            let path = file.path();
-            let pathname = path
-                .file_name()
-                .unwrap()
-                .to_str()
-                .map(|s| s.to_owned())
-                .unwrap();
-            if path.is_file() {
-                file_names.push(pathname)
-            } else if path.is_dir() {
-                dir_names.push(pathname)
+impl Default for InnerAppState {
+    fn default() -> Self {
+        return {
+            InnerAppState {
+                files: Vec::new(),
+                active_dir: None,
             }
-        }
+        };
     }
-
-    return (file_names, dir_names);
-}
-
-#[tauri::command]
-fn open_file(filepath: String, state: State<AppState>) {
-    let rope = ropey::Rope::from_reader(File::open(&filepath).unwrap()).unwrap();
-
-    state.files.lock().unwrap().push(OpenFiles {
-        filepath,
-        rope: Mutex::new(rope),
-    });
-}
-
-#[tauri::command]
-fn file_lines(
-    file_index: usize,
-    mut start_pos: usize,
-    end_pos: usize,
-    state: State<AppState>,
-) -> Vec<String> {
-    let files = state.files.lock().unwrap();
-    let file = files.get(file_index).unwrap();
-
-    let mut lines: Vec<String> = vec![];
-    let binding = file.rope.lock().unwrap();
-    if let Some(mut line) = binding.get_lines_at(start_pos) {
-        while start_pos < end_pos {
-            let next_line = line.next();
-            if next_line == None {
-                break;
-            }
-            lines.push(next_line.expect("Error reading file").to_string());
-            start_pos += 1;
-        }
-    }
-
-    lines
-}
-
-#[tauri::command]
-fn add_chars(
-    file_index: usize,
-    chars: String,
-    start_line: usize,
-    start_point: usize,
-    state: State<AppState>,
-) {
-    let files = state.files.lock().unwrap();
-    let file = files.get(file_index).unwrap();
-    let line_index = file.rope.lock().unwrap().line_to_char(start_line);
-    let line_end_index = file.rope.lock().unwrap().line_to_char(start_line + 1);
-
-    let mut new_line = file.rope.lock().unwrap().line(start_line).to_string();
-
-    new_line.insert_str(start_point, &chars);
-
-    file.rope.lock().unwrap().remove(line_index..line_end_index);
-    file.rope.lock().unwrap().insert(line_index, &new_line);
-
-    file.rope
-        .lock()
-        .unwrap()
-        .write_to(File::create(&files[file_index].filepath).unwrap())
-        .unwrap();
 }
 
 fn main() {
-    let app_state = AppState {
-        files: Mutex::new(vec![]),
-        active_dir: None,
-    };
+    let app_state = Mutex::new(InnerAppState::default());
 
     tauri::Builder::default()
         .manage(app_state)
